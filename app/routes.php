fcc2dc6e5dc39ba8e63804c00eedeb63f06e16f6
Jesse O'Brien <jesse@jesse-obrien.ca>
2013-06-20 13:58:32 -0400

    Adding 404 page

--- app/routes.php
+++ app/routes.php
@@ -5,3 +5,7 @@ Route::get('/', 'HomeController@index');
 Route::get('/laravel_log', function(){
 	return View::make('laravel');
 });
+
+App::missing(function($exception){
+	return Response::view('404', array(), 404);
+});
---
+++ app/views/404.blade.php
@@ -0,0 +1,9 @@
+@extends('template')
+
+@section('content')
+<div class="row">
+	<div class="large-4 large-centered columns">
+		<h3>404 - Page Not Found.</h3>
+	</div>
+</div>
+@stop
